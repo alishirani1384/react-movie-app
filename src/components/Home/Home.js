14bcb3608452485a891e74592d63655ec877e854
Ali Shirani <alishiranii1384@gmail.com>
2022-07-16 21:47:57 +0430

    five

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,15 @@
 
 import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing';
-import { useDispatch } from 'react-redux';
-import { fetchAsyncMovies, fetchAsyncSeries } from '../../features/movies/movieSlice';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAsyncMovies, fetchAsyncSeries, getAllMovies } from '../../features/movies/movieSlice';
+import ReactLoading from 'react-loading';
 
 
 
 const Home = () => {
   const dispatch = useDispatch();
+  const data = useSelector(getAllMovies)
  
   useEffect(() => {
     dispatch(fetchAsyncMovies())
@@ -17,10 +18,20 @@ const Home = () => {
 
   return (
     <>
-      <div className='banner-img'></div>
-      <MovieListing />
+      <div className="banner-img"></div>
+      {Object.keys(data).length === 0 ? (
+        <ReactLoading
+          className="loading"
+          type={"bubbles"}
+          color={"blue"}
+          height={"30%"}
+          width={"90%"}
+        />
+      ) : (
+        <MovieListing />
+      )}
     </>
-  )
+  );
 }
 
 export default Home
--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAsyncMovieDetail, getSelectedMovie, removeSelectedMovie } from '../../features/movies/movieSlice';
-import "./MovieDetails.scss"
+import "./MovieDetails.scss";
+import ReactLoading from "react-loading";
 const MovieDetails = () => {
   const { imdbID } = useParams()
   const dispatch = useDispatch()
@@ -17,53 +18,55 @@ const MovieDetails = () => {
   },[dispatch,imdbID])
   return (
     <div className="movie-section">
-      {Object.keys(data).length === 0 ?
-    (<div>...Loading</div>):(
-      <>
-      <div className="section-left">
-        <h1 className="movie-title">{data.Title}</h1>
-        <div className="movie-rating">
-          <span>
-            IMDB Rating <i className="fa fa-star"></i>:{data.imdbRating}
-          </span>
-          <span>
-            IMDB Votes <i className="fa fa-thumbs-up"></i>:{data.imdbVotes}
-          </span>
-          <span>
-            Runtime <i className="fa fa-film"></i>:{data.Runtime}
-          </span>
-          <span>
-            Year <i className="fa fa-calendar"></i>:{data.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{data.plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+      {Object.keys(data).length === 0 ? (
+        <ReactLoading type={'spin'} color={'blue'} height={"20%"} width={"20%"} />
+      ) : (
+        <>
+          <div className="section-left">
+            <h1 className="movie-title">{data.Title}</h1>
+            <div className="movie-rating">
+              <span>
+                IMDB Rating <i className="fa fa-star"></i>:{data.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <i className="fa fa-thumbs-up"></i>:{data.imdbVotes}
+              </span>
+              <span>
+                Runtime <i className="fa fa-film"></i>:{data.Runtime}
+              </span>
+              <span>
+                Year <i className="fa fa-calendar"></i>:{data.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{data.plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Genre</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Languages</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
+          <div className="section-right">
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Genre</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Languages</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={data.Poster} alt={data.Title} />
-        </div>
-      </>)}
+        </>
+      )}
     </div>
   );
 }
